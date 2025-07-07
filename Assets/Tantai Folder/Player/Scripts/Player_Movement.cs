5081429b1ca656dbf680c23576f89fdbe4642494
Tantai07 <167741192+Tantai07@users.noreply.github.com>
2025-07-07 16:56:21 +0700

    Player Movement

---
+++ Assets/Tantai Folder/Player/Scripts/Player_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] private float move_Speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 move_Input;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = move_Input.normalized * move_Speed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        move_Input = context.ReadValue<Vector2>();
+    }
+}
